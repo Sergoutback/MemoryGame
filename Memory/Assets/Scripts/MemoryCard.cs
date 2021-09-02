77b40d7babd64b9d9583685c656e91fcfe3c4746
Sergoutback <42220229+Sergoutback@users.noreply.github.com>
2021-09-02 16:00:27 +0300

    02092021_1600
    yet finished, but some bugs

--- Memory/Assets/Scripts/MemoryCard.cs
+++ Memory/Assets/Scripts/MemoryCard.cs
@@ -5,6 +5,7 @@ public class MemoryCard : MonoBehaviour
 {
 	[SerializeField] private GameObject cardBack;
 	[SerializeField] private SceneController controller;
+	
 	private int _id;
 	public int id
 	{
@@ -15,12 +16,18 @@ public class MemoryCard : MonoBehaviour
 		_id = id;
 		GetComponent<SpriteRenderer>().sprite = image;
 	}
-
+	
 	public void OnMouseDown()
 	{
-		if (cardBack.activeSelf)
+		if (cardBack.activeSelf && controller.canReveal)
 		{
 			cardBack.SetActive(false);
+			controller.CardRevealed(this);
 		}
+	}
+
+	public void Unreveal()
+	{
+		cardBack.SetActive(true);
 	}
 }
